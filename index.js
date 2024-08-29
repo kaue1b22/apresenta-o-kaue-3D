a083fe68b6f52cfba91e0a05eba3faa7e86d58e0
kaue1b22 <128189073+kaue1b22@users.noreply.github.com>
2024-08-29 01:26:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Console } = require("console");
+
+const primeiroNome = "kaue";
+const sobreNome = "henrique";
+const idade = 16;
+const altura = 1.69;
+const massa = 65;
+const cidadeNatal = "são josé";
+
+console.log("meu primeiro nome é:");
+console.log(primeiroNome);
+console.log("meu sobre nome é:");
+console.log(sobreNome);
+console.log("minha idade é de:");
+console.log(idade);
+console.log("minha altura é de:");
+console.log(altura);
+console.log("meu peso é de:");
+console.log(massa);
+console.log("e minha cidade natal é:")
+console.log(cidadeNatal);
